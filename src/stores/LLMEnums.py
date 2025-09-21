a8b519c43a3270d50cf3020cdd22b50730ec4282
AhmadEwidat <ahmadrealma7@gmail.com>
2025-09-22 00:08:50 +0300

    build LLM interface,Enum, OpenAI Provider

---
+++ src/stores/LLMEnums.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class LLMEnums(str, Enum):
+    OPENAI = "openai"
+    COHERE = "cohere"
+    ANTHROPIC = "anthropic"
+    AZURE_OPENAI = "azure_openai"
+    COHERE = "cohere"
+    AI21 = "ai21"
+    LLMOXY = "llmoxy"
+    CUSTOM = "custom"
+    LOCAL = "local"
+    HUGGINGFACE = "huggingface"
+    VERCEL_AI = "vercel_ai"
+    MISTRAL = "mistral"
+    STARCODER = "starcoder"
+    GOOGLE_PALM = "google_palm"
+    ALPHACODE = "alphacode"
+    LLAMA2 = "llama2"
+    CLAUDE = "claude"
+    GEMINI = "gemini"
+    OTHER = "other"
+class OpenAIEnums(Enum):
+    System ="system"
+    User="user"
+    ASSISTANT="assistant"
